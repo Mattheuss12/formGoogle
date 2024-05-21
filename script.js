40259d7685ea99a527fe9a742a8906e2e16c9184
Matheus Almeida <theus850@gmail.com>
2024-05-21 17:30:31 -0300

    feat: adjustment on submit button

--- script.js
+++ script.js
@@ -1,35 +1,33 @@
-document
-  .getElementById("reservationForm")
-  .addEventListener("submit", function (event) {
-    event.preventDefault(); 
+document.getElementById("reservationForm").addEventListener("submit", function(event) {
+  event.preventDefault(); 
 
-    var formData = {
-      qtdMesa: document.querySelector('input[name="qtdMesa"]:checked').value,
-      garcom: document.getElementById("garcom").value,
-    };
+  var submitBtn = document.getElementById("submitBtn");
+  submitBtn.disabled = true; 
 
-    
-    fetch(
-      "https://script.google.com/macros/s/AKfycbwv9a-DCJGid54Hd6ZBUvXT3meIppPn6CSJrOOHivSxviZsru_zZIzHNgRXqX8daVMD7w/exec",
-      {
-        method: "POST",
-        body: JSON.stringify(formData),
-      }
-    )
-      .then((response) => {
-        if (response.ok) {
-          alert("Formulário enviado com sucesso!");
-          document.getElementById("reservationForm").reset(); 
-        } else {
-          throw new Error(
-            "Erro ao enviar o formulário. Por favor, tente novamente."
-          );
-        }
-      })
-      .catch((error) => {
-        console.error("Erro ao enviar formulário:", error);
-        alert(
-          "Ocorreu um erro ao enviar o formulário. Por favor, tente novamente."
-        );
-      });
+  var formData = {
+    qtdMesa: document.querySelector('input[name="qtdMesa"]:checked').value,
+    garcom: document.getElementById("garcom").value
+  };
+
+  
+  fetch("https://script.google.com/macros/s/AKfycbwv9a-DCJGid54Hd6ZBUvXT3meIppPn6CSJrOOHivSxviZsru_zZIzHNgRXqX8daVMD7w/exec", {
+    method: "POST",
+    body: JSON.stringify(formData)
+  })
+  .then(response => {
+    if (response.ok) {
+      alert("Formulário enviado com sucesso!");
+      document.getElementById("reservationForm").reset(); 
+    } else {
+      throw new Error("Erro ao enviar o formulário. Por favor, tente novamente.");
+    }
+  })
+  .then(() => {
+    submitBtn.disabled = false; 
+  })
+  .catch(error => {
+    console.error("Erro ao enviar formulário:", error);
+    alert("Ocorreu um erro ao enviar o formulário. Por favor, tente novamente.");
+    submitBtn.disabled = false;
   });
+});
