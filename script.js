8ba60b0c6044fee3d54acedb478b1f03152798c0
Matheus Almeida <theus850@gmail.com>
2024-05-26 14:18:00 -0300

    feat: implementação layout

--- script.js
+++ script.js
@@ -1,41 +1,48 @@
-document
-  .getElementById("reservationForm")
-  .addEventListener("submit", function (event) {
-    event.preventDefault();
+document.getElementById("reservationForm").addEventListener("submit", function (event) {
+  event.preventDefault();
 
-    var submitBtn = document.getElementById("submitBtn");
-    submitBtn.disabled = true;
+  var submitBtn = document.getElementById("submitBtn");
+  submitBtn.disabled = true;
 
-    var formData = {
-      qtdMesa: document.querySelector('input[name="qtdMesa"]:checked').value,
-      garcom: document.getElementById("garcom").value,
-    };
+  var formData = {
+    name: document.getElementById("name").value,
+    firstTime: document.querySelector('input[name="firstTime"]:checked').value,
+    preference: document.getElementById("preference").value,
+    avaliation: document.getElementById("avaliation").value,
+    dessert: document.getElementById("dessert").value,
+    contact: document.getElementById("contact").value
+  };
 
-    fetch(
-      "https://script.google.com/macros/s/AKfycbybHcMinUu39mS3dEAk46W78U0QOcSTKo5qkQbeSFok6H2AgPolX5LE6T1v6qTYLVYCEg/exec",
-      {
-        method: "POST",
-        body: JSON.stringify(formData),
+  fetch("https://script.google.com/macros/s/AKfycbwgbvzRH8lbYf1pteTs0mbY8l_NVEZ3918K-qBRzhbAd88Y-UBdX9ZRaLc_S5rTwMtOgQ/exec", {
+    method: "POST",
+    body: JSON.stringify(formData),
+  })
+    .then((response) => {
+      if (response.ok) {
+        var popup = document.getElementById('popup');
+        popup.style.display = 'block';
+
+        document.querySelector('.close-btn').addEventListener('click', function() {
+          popup.style.display = 'none';
+        });
+
+        window.addEventListener('click', function(event) {
+          if (event.target == popup) {
+            popup.style.display = 'none';
+          }
+        });
+
+        document.getElementById("reservationForm").reset();
+      } else {
+        throw new Error("Erro ao enviar o formulário. Por favor, tente novamente.");
       }
-    )
-      .then((response) => {
-        if (response.ok) {
-          alert("Formulário enviado com sucesso!");
-          document.getElementById("reservationForm").reset();
-        } else {
-          throw new Error(
-            "Erro ao enviar o formulário. Por favor, tente novamente."
-          );
-        }
-      })
-      .then(() => {
-        submitBtn.disabled = false;
-      })
-      .catch((error) => {
-        console.error("Erro ao enviar formulário:", error);
-        alert(
-          "Ocorreu um erro ao enviar o formulário. Por favor, tente novamente."
-        );
-        submitBtn.disabled = false;
-      });
-  });
+    })
+    .then(() => {
+      submitBtn.disabled = false;
+    })
+    .catch((error) => {
+      console.error("Erro ao enviar formulário:", error);
+      alert("Ocorreu um erro ao enviar o formulário. Por favor, tente novamente.");
+      submitBtn.disabled = false;
+    });
+});
